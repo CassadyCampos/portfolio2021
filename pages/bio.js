211b3812fbfa3036966cee7f6b25ccea5ffcfb31
Cassady <cassady801@gmail.com>
2021-12-02 23:06:32 -0700

    First Commit

---
+++ pages/bio.js
@@ -0,0 +1,10 @@
+const Bio = () => {
+    return (
+        <div className="mt-3">
+            <p className="display-4 text-center">I'm Ebenezer Scrouge</p>
+            <p className="text-center">Ebenezer Scrouge's Bio</p>
+        </div>
+    )
+}
+
+export default Bio;
