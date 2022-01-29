4e481c03e72947af29a74e8a65a90bcb9e3c809b
Cassady <cassady801@gmail.com>
2022-01-28 21:35:22 -0700

    update

--- pages/now.js
+++ pages/now.js
@@ -11,18 +11,18 @@ const NowPage = () => {
             <br/>
 
             <div className={styles.baseText}>
-                {"Since then, I have been working at Arcurve as a Full-Stack Software Engineer. Here I was building a CRM web app for prominent figures in the Oil and Energy sector. Monitoring the logistics of fuel deliveries, products, and retail gas locations across the United States and Canada."}
+                {"Since then, I have been working as a Full-Stack Software Engineer at Arcurve. Building a CRM web app for prominent figures in the Oil and Energy sector. Monitoring logistics of fuel deliveries, products, and retail gas locations across the United States and Canada."}
             </div>
             <br/>
 
             <div className={styles.baseText}>
-                {"Personally, at the moment I'm really into Web3 and decentralized web apps (DAPPS). I'm also looking to get into open source projects, however I'm still looking to find the right project."}
+                {"My current interests are Web3, and blockchain, and web technologies. I'm into reading books, and learning about random things that I can write about."}
             </div>
             <br/>
 
             <div className={styles.baseText}>{"Thanks to Ryan Kulp for the inspiration!"}</div>
             <div className={styles.baseText}>Like the idea of a 
-            <a href="https://nownownow.com/about" target="_blank" rel="noreferrer">{"'Now'"}</a>
+            <a href="https://nownownow.com/about" target="_blank" rel="noreferrer">{" 'Now' "}</a>
              {"page? Thank David!"}</div>
         </div>
     )
