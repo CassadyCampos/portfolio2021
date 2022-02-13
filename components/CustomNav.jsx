91bf4c0f2afeebd38ff2ee27fc7106b30b7e05c2
Cassady <cassady801@gmail.com>
2022-02-12 20:24:31 -0700

    quick link and style navbar

--- components/CustomNav.jsx
+++ components/CustomNav.jsx
@@ -3,25 +3,27 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Container from 'react-bootstrap/Container'
+import styles from '../styles/Home.module.css';
 // import './navbar.css';
 class CustomNav extends Component {
   render() {
     return (
 <Navbar bg="light" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home" className={styles.baseFont}>Cassady Campos</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <Nav.Link href="/now">Now</Nav.Link>
+        <Nav.Link href="/posts">Posts</Nav.Link>
+        <Nav.Link href="/projects">Projects</Nav.Link>
+        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="/now">Now</NavDropdown.Item>
+          <NavDropdown.Item href="/posts">Posts</NavDropdown.Item>
+          <NavDropdown.Item href="/projects">Projects</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
+        </NavDropdown> */}
       </Nav>
     </Navbar.Collapse>
   </Container>
