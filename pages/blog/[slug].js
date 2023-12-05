27e9fcae276cf3c7985db0ffad446b074cf161ff
cassadycampos <cassady801@gmail.com>
2023-12-04 22:01:21 -0700

    revert

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -29,7 +29,6 @@ const PostPage = ({ frontMatter: { title, date, thumbnailUrl }, mdxSource }) => 
                     height={360}
                     objectFit="cover"
                 />
-
                 <div className={styles.dateText + ' animate__animated animate__slideInUp'}>{date}</div>
                 <br/>
                 <div className={styles.baseFont + ' ' +  styles.baseText + ' animate__animated animate__slideInUp'}>
